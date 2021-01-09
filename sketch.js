46bf244d8975757135b52cae73de559afdf01a89
masha007555 <71925958+masha007555@users.noreply.github.com>
2021-01-09 16:55:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+
+var allPlayers;
+var database;
+
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+
+  if (playerCount=== 4){
+
+    game.update(1);
+  }
+
+  if (gameState===1){
+
+    clear();
+    game.play();
+  }
+}
+
+
+
+
+// var canvas, backgroundImage;
+// var allPlayers;
+// var gameState = 0;
+// var playerCount;
+
+// var database;
+
+// var form, player, game;
+
+
+// function setup(){
+//   canvas = createCanvas(400,400);
+//   database = firebase.database();
+//   game = new Game();
+//   game.getState();
+//   game.start();
+// }
+
+
+// function draw(){
+
+//   if (playerCount === 4) {
+//     game.update(1);
+//   }
+//   if (gameState === 1) {
+//     clear();
+//     game.play();
+//   }
+
+  
+// }
+ 
